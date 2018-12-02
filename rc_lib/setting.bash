4b5328636bdb0a39e60c7ac4b150533fc00c9f38
miracle <miracle@ik1-340-30365.vs.sakura.ne.jp>
2018-12-02 13:53:46 +0900

    rcの基本ファイルを作成

---
+++ rc_lib/setting.bash
@@ -0,0 +1,21 @@
+export PS1="[\u@\h:\w]\$ "
+
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
+}
+function promps {
+  # 色は気分で変えたいかもしれないので変す宣言しておく
+  local  BLUE="\[\e[1;34m\]"
+  local  RED="\[\e[1;31m\]"
+  local  GREEN="\[\e[1;32m\]"
+  local  WHITE="\[\e[00m\]"
+  local  GRAY="\[\e[1;37m\]"
+
+  case $TERM in
+    xterm*) TITLEBAR='\[\e]0;\W\007\]';;
+    *)      TITLEBAR="";;
+  esac
+  local BASE="${WHITE}\u@\h:${GREEN}\w"
+  PS1="${TITLEBAR}${BASE}${WHITE}\$(parse_git_branch)${WHITE}\$${WHITE} "
+}
+promps
