8a4739caf327d9425a48bd10a1deb42e9774f5b3
setsumaru <setsumaru@moyamoya.space>
2018-12-09 20:09:45 +0900

    zsh設定ファイル追加

---
+++ rc_lib/setting.zsh
@@ -0,0 +1,32 @@
+# https://qiita.com/tonosama/items/50c043ebc89009ad95be
+
+# Lines configured by zsh-newuser-install
+export EDITORP=vim #エディタをvimに設定
+export LANG=ja_JP.UTF-8 #文字コードをUTF-8に設定
+
+# history
+HISTFILE=~/.zsh_hist
+HISTSIZE=1000
+SAVEHIST=1000
+setopt extended_history #ヒストリに実行時間も保存
+setopt hist_ignore_dups #直前と同じコマンドはヒストリに追加しない
+
+# <Tab>でパス名の補完候補を表示したあと、
+# 続けて<Tab>を押すと候補からパス名を選択することができるようになる
+zstyle ':completion:*:default' menu select=1
+
+# cdの後にlsを実行
+chpwd() { ls -latrhG  }
+
+# https://qiita.com/ryutoyasugi/items/cb895814d4149ca44f12
+# prompt
+autoload -Uz vcs_info
+setopt prompt_subst
+zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' stagedstr "%F{magenta}!"
+zstyle ':vcs_info:git:*' unstagedstr "%F{yellow}+"
+zstyle ':vcs_info:*' formats "%F{cyan}%c%u[%b]%f"
+zstyle ':vcs_info:*' actionformats '[%b|%a]'
+precmd() { vcs_info }
+PROMPT='%m:%F{green}%~%f %n %F{yellow}$%f '
+RPROMPT='${vcs_info_msg_0_}'
