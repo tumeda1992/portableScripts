5e7f1e3d1b8acea1ce896c3c5db944a5e3e75671
Takahiro Umeda <takahiro.umeda@pixta.co.jp>
2018-12-26 10:49:22 +0900

    gl1のエラー解消

--- rc_lib/parts_config/alias.sh
+++ rc_lib/parts_config/alias.sh
@@ -3,7 +3,7 @@ alias gdh='git diff HEAD^..HEAD'
 alias gs='git status'
 alias gc='git checkout'
 alias gpull='git pull origin'
-alias gl1='git log --online --graph --decorate'
+alias gl1='git log --oneline --graph --decorate'
 
 
 
