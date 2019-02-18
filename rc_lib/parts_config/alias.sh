750b99dc8fa0a62b2d74ac21056a4a2e1a2caa3e
Takahiro Umeda <takahiro.umeda@pixta.co.jp>
2019-02-18 10:41:21 +0900

    gpullの実装途中

--- rc_lib/parts_config/alias.sh
+++ rc_lib/parts_config/alias.sh
@@ -1,17 +1,21 @@
 # alias r='ruby' # ビルトインと競合する
+
+# 初期起動時のブランチをキャッシュしてしまうのでaliasで設定したコマンド実行時に毎回取得するようにした
+# currentBranch=$(git branch | grep '\*'  | awk '{print $2}')
 # git alias
 alias gdh='git diff HEAD^..HEAD'
 alias gs='git status'
 alias gc='git checkout'
-alias gpull='git pull origin'
+#gpullCmd="git pull origin $(curBranch);"
+curBranch(){echo $(git branch | grep '\*'  | awk '{print $2}')}
+#alias gpull="echo 'git pull origin $(curBranch)' ; sh -c 'git pull origin $(curBranch)'"
+alias gpull="git pull origin"
 alias gl1='git log --oneline --graph --decorate'
 
 # ruby/rails alias
 alias rs='bundle exec rails'
 
-
-
-
+# TODO aliasでのgpull廃止。aliasにcurBranchのキャッシュ残るから。globalスクリプトディレクトリを作成してそこにパスを通し、どこでも実行できるように
 
 
 
