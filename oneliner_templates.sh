162321aead1502066b4798b6335ee7869eefba05
Takahiro Umeda <takahiro.umeda@pixta.co.jp>
2020-03-12 01:55:19 +0900

    よく使うコマンドのテンプレートファイル作成

---
+++ oneliner_templates.sh
@@ -0,0 +1,2 @@
+# ファイル名と内容を出力
+for file in $(ls *.txt); do echo $file; cat $file; done
---
+++ shell_script_template.sh
@@ -0,0 +1,17 @@
+# for文
+for i in $(seq 10)
+do
+echo "test"
+done
+## ワンライナー
+for i in $(seq 10);do echo "test"; done
+
+
+
+# if文
+
+
+## empty?系
+
+
+# sed
