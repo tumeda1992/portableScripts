f5f9c225375a1571e4f67e3b7a84bef145e6850a
Takahiro Umeda <takahiro.umeda@pixta.co.jp>
2020-03-17 14:57:44 +0900

    便利メソッドをモジュール化

--- frequent_use_codes/ruby_file_handlers/read_and_write_csv.rb
+++ frequent_use_codes/ruby_file_handlers/read_and_write_csv.rb
@@ -17,47 +17,61 @@ def main(csv)
   # output_file_writer.close
 end
 
-def csv_foreach(csv)
-  log "#{Time.now}: read start #{csv}"
-  all_line_count = line_count(csv)
+module FileHandler
+  class << self
+    def csv_foreach(csv)
+      log "#{Time.now}: read start #{csv}"
+      all_line_count = line_count(csv)
 
-  return_values = CSV.foreach(csv, headers: true).with_index(1).map do |row, row_no|
-    log progress(line_no, all_line_count) if progress_timing?(all_line_count, row_no)
-    yield(row)
+      return_values = CSV.foreach(csv, headers: true).with_index(1).map do |row, row_no|
+        log progress(line_no, all_line_count) if progress_timing?(all_line_count, row_no)
+        yield(row)
+      end
+
+      log "#{Time.now}: read end #{csv}"
+
+      return_values
+    end
+
+    def line_count(file)
+      open(file){|f|
+        while f.gets; end
+        f.lineno
+      }
+    end
+
+    private
+
+    def log(message)
+      puts(message) if $is_debug
+    end
+
+    def progress_timing?(all_line_count, line_no)
+      return false if all_line_count < 100
+
+      # NOTE 処理時間によって変更
+      div_number = 100
+
+      percent_unit = all_line_count / div_number
+      line_no % percent_unit == 0
+    end
+
+    def progress(current_count, all_count)
+      finished_rate = current_count.fdiv(all_count)
+      finished_rate_percent = (finished_rate * 100).round(2)
+      "#{Time.now}: #{finished_rate_percent}% (#{current_count}/#{all_count})"
+    end
   end
-
-  log "#{Time.now}: read end #{csv}"
-
-  return_values
 end
 
-def log(message)
-  puts(message) if $is_debug
+module CommonUtilities
+  class << self
+    def percent(num, all_count)
+      (num.fdiv(all_count) * 100).round(2)
+    end
+  end
 end
 
-def progress_timing?(all_line_count, line_no)
-  return false if all_line_count < 100
-  div_number = 100
 
-  percent_unit = all_line_count / div_number
-  line_no % percent_unit == 0
-end
-
-def progress(current_count, all_count)
-  finished_rate = current_count.fdiv(all_count)
-  finished_rate_percent = (finished_rate * 100).round(2)
-  "#{Time.now}: #{finished_rate_percent}% (#{current_count}/#{all_count})"
-end
-
-def line_count(file)
-  open(file){|f|
-    while f.gets; end
-    f.lineno
-  }
-end
-
-def percent(num, all_count)
-  (num.fdiv(all_count) * 100).round(2)
-end
 
 main(ARGV[0])
--- frequent_use_codes/ruby_file_handlers/read_and_write_file.rb
+++ frequent_use_codes/ruby_file_handlers/read_and_write_file.rb
@@ -3,7 +3,7 @@ $is_debug = true
 def main(file)
   # output_file_writer = File.open(output_file)
 
-  file_foreach(file) do |line|
+  FileHandler.file_foreach(file) do |line|
     p line
 
     # output_file.put(line)
@@ -12,44 +12,61 @@ def main(file)
   # output_file_writer.close
 end
 
-def file_foreach(file)
-  File.open(file) do |lines|
-    log "#{Time.now}: read start #{file}"
-    all_line_count = lines.size
+module FileHandler
+  class << self
+    def file_foreach(file)
+      File.open(file) do |lines|
+        log "#{Time.now}: read start #{file}"
+        all_line_count = lines.size
 
-    return_values = lines.each_line.each.with_index(1).map do |line, line_no|
-      log progress(line_no, all_line_count) if progress_timing?(all_line_count, line_no)
-      yield(line.chomp)
+        return_values = lines.each_line.each.with_index(1).map do |line, line_no|
+          log progress(line_no, all_line_count) if progress_timing?(all_line_count, line_no)
+          yield(line.chomp)
+        end
+
+        log "#{Time.now}: read end #{file}"
+
+        return_values
+      end
     end
 
-    log "#{Time.now}: read end #{file}"
+    def line_count(file)
+      open(file){|f|
+        while f.gets; end
+        f.lineno
+      }
+    end
 
-    return_values
+    private
+
+    def log(message)
+      puts(message) if $is_debug
+    end
+
+    def progress_timing?(all_line_count, line_no)
+      return false if all_line_count < 100
+
+      # NOTE 処理時間によって変更
+      div_number = 100
+
+      percent_unit = all_line_count / div_number
+      line_no % percent_unit == 0
+    end
+
+    def progress(current_count, all_count)
+      finished_rate = current_count.fdiv(all_count)
+      finished_rate_percent = (finished_rate * 100).round(2)
+      "#{Time.now}: #{finished_rate_percent}% (#{current_count}/#{all_count})"
+    end
   end
 end
 
-def log(message)
-  puts(message) if $is_debug
-end
-
-def progress_timing?(all_line_count, line_no)
-  return false if all_line_count < 100
-
-  # NOTE 処理時間によって変更
-  div_number = 100
-
-  percent_unit = all_line_count / div_number
-  line_no % percent_unit == 0
-end
-
-def progress(current_count, all_count)
-  finished_rate = current_count.fdiv(all_count)
-  finished_rate_percent = (finished_rate * 100).round(2)
-  "#{Time.now}: #{finished_rate_percent}% (#{current_count}/#{all_count})"
-end
-
-def percent(num, all_count)
-  (num.fdiv(all_count) * 100).round(2)
+module CommonUtilities
+  class << self
+    def percent(num, all_count)
+      (num.fdiv(all_count) * 100).round(2)
+    end
+  end
 end
 
 main(ARGV[0])
