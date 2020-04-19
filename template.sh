b0afec03f457236aa310350b901994558a87cac6
setsumaru1992 <setsumaru@moyamoya.space>
2020-04-19 17:11:02 +0900

    create_shell_script.shをブラッシュアップ

---
+++ template.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+#（オプション）このファイルを作業ディレクトリとする
+script_exec_path=$(pwd)
+this_script_dir=$(dirname $0)
+#cd $this_script_path
+
+##（オプション）オプション引数を使う場合は以下を使用
+#haveOpt=0
+#while getopts f:dc: OPT
+#do
+#  case $OPT in
+#    f) filename="$OPTARG"
+#      haveOpt=1
+#      ;;
+#    d) isDebug=1
+#      haveOpt=1
+#      ;;
+#    c) count='$OPTARG'
+#      haveOpt=1
+#      ;;
+#  esac
+#done
+#
+#if [ ${haveOpt} = 1 ]
+## オプションがある場合、オプション分だけ引数をずらす（これがないと-fなどが$1に入る）
+#then
+#  shift $(($OPTIND - 1))
+#fi
+
+main () {
+  hoge=$1
+}
+
+
+
+main $1
